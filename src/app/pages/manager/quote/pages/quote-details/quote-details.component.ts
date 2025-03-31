7e935979d9bb2134991fcf1353adf0cd73341d23
jeremie04 <rantojeremie@gmail.com>
2025-03-31 15:50:17 +0300

    email-fixing

--- src/app/pages/manager/quote/pages/quote-details/quote-details.component.ts
+++ src/app/pages/manager/quote/pages/quote-details/quote-details.component.ts
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
 import { DatePicker } from 'primeng/datepicker';
 import { FloatLabel } from 'primeng/floatlabel';
 import { MontantPipe } from "../../../../../shared/pipes/montant.pipe";
+import { EmailService } from '../../../../../core/service/email.service';
 
 @Component({
   selector: 'app-quote-details',
@@ -57,18 +58,19 @@ export class QuoteDetailsComponent {
     addDiscountVisible: boolean = false;
     discount: number = 0;
     showUpdateDate: boolean = false;
+    newDate: any;
 
 	constructor (
 		private quoteService: QuoteService, 
 		private route: ActivatedRoute, 
 		private router: Router,
 		private confirmationService: ConfirmationService,
-		private messageService: MessageService
+		private messageService: MessageService,
+        private emailService: EmailService
 	) {}
 
     ngOnInit() {
         const id = this.route.snapshot.paramMap.get('id');
-		console.log(id);
 		
 		if (id) {
 			this.getDetailsQuote(id);
@@ -83,10 +85,10 @@ export class QuoteDetailsComponent {
 			next : (data) => {
 				this.quote = data;
                 this.discount = this.quote.discount;
-                if (this.quote.appointment_id.time_start && typeof this.quote.appointment_id.time_start === 'string') {
-                    this.quote.appointment_id.time_start = new Date(this.quote.appointment_id.time_start);
+                if (this.quote.appointment.time_start && typeof this.quote.appointment.time_start === 'string') {
+                    this.quote.appointment.time_start = new Date(this.quote.appointment.time_start);
                 }
-				console.log(this.quote.appointment_id.time_start);
+                this.newDate = data.appointment.time_start;
 			}, error : (error) => {
 				this.router.navigate([`manager/error`, error.error.message]);
 			}
@@ -113,6 +115,17 @@ export class QuoteDetailsComponent {
             },
             error: (error:any) => {
 				this.messageService.add({ severity: 'error', summary: 'Une erreur est survenue', detail: error.error.message, life: 3000 });
+            }
+        });
+    }
+
+    SendEmailConfirmationNewDate() {
+        this.emailService.sendEmailConfirmation(this.quote.appointment.user.email, this.newDate, this.quote.appointment.time_start, this.quote._id).subscribe({
+            next: () => {
+                this.messageService.add({ severity: 'info', summary: 'Email envoyé', detail: 'Un email de confirmation a été envoyé', life: 3000 });
+            },
+            error: (error:any) => {
+                this.messageService.add({ severity: 'error', summary: 'Une erreur est survenue', detail: error.error.message, life: 3000 });
             }
         });
     }
@@ -153,6 +166,7 @@ export class QuoteDetailsComponent {
                     this.addDiscountVisible = false;
                 } else if (type === 'newDate') {
                     this.showUpdateDate = false;
+                    this.SendEmailConfirmationNewDate();
                 }
             },
             reject: () => { }
